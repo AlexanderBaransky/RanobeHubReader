8089001abb475c06fee331417f06259e7eb1ea9f
AlexanderBaransky <alexander.baranskiy@yandex.ru>
2021-06-26 17:05:40 +0300

    last_update_item.xml: Add Volumes and Chapters count LastUpdatesAdapter: Add logics to fill new TextViews, add rounded corners to poster

--- app/src/main/java/com/sanyapilot/ranobehubreader/lastUpdates/LastUpdatesAdapter.kt
+++ app/src/main/java/com/sanyapilot/ranobehubreader/lastUpdates/LastUpdatesAdapter.kt
@@ -7,21 +7,36 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.sanyapilot.ranobehubreader.MainActivity
 import com.sanyapilot.ranobehubreader.R
 
 class LastUpdatesAdapter : RecyclerView.Adapter<LastUpdatesAdapter.ViewHolder>() {
     private lateinit var dataSet : LastUpdatesResponseModel
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val lastUpdateTitle: TextView = view.findViewById(R.id.last_update_title)
-        private val lastUpdateSummary: TextView = view.findViewById(R.id.last_update_summary)
-        private val lastUpdatePoster: ImageView = view.findViewById(R.id.last_update_poster)
-        private val lastUpdateLikes: TextView = view.findViewById(R.id.last_update_likes_count)
+        private val currView = view
+        private val lastUpdateTitle : TextView = view.findViewById(R.id.last_update_title)
+        private val lastUpdateSummary : TextView = view.findViewById(R.id.last_update_summary)
+        private val lastUpdatePoster : ImageView = view.findViewById(R.id.last_update_poster)
+        private val lastUpdateLikes : TextView = view.findViewById(R.id.last_update_likes_count)
+        private val lastUpdateVolumes : TextView = view.findViewById(R.id.last_update_volumes_count)
+        private val lastUpdateChapters : TextView = view.findViewById(R.id.last_update_chapter_count)
         fun bind(data: Resource) {
-            Glide.with(lastUpdatePoster).load(data.poster.medium).into(lastUpdatePoster)
+            val context = currView.context
+            Glide.with(context)
+                .load(data.poster.medium).transform(
+                    FitCenter(),
+                    RoundedCorners(context.resources.getDimensionPixelSize(R.dimen.corner_radius)))
+                .into(lastUpdatePoster)
+
             lastUpdateTitle.text = data.names.rus
             lastUpdateSummary.text = data.synopsis
             lastUpdateLikes.text = data.rating.toString()
+            lastUpdateVolumes.text = data.counts.volumes.dropLast(4)
+            lastUpdateChapters.text = data.counts.chapters.dropLast(5)
         }
     }
 
